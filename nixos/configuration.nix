be6fa8d52ec22e4fd504b7b8c6fd8cb0afd5822f
Alikay <1401288@gmail.com>
2024-04-15 13:07:37 -0600

    Automatic commit from rebuild script

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -232,7 +232,7 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
      foot
-     #aseprite
+     pkgs-stable.aseprite
      bear
      btop
      blueberry
