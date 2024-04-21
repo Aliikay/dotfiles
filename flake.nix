0614856bf5148690091f27923ea9af49c7b8d412
Alikay <1401288@gmail.com>
2024-04-20 19:50:21 -0600

    fix

--- flake.nix
+++ flake.nix
@@ -71,7 +71,7 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.extraSpecialArgs = {inherit inputs; };
-          home-manager.users.alikay = import ./home.nix;
+          home-manager.users.alikay = import ./nixos/home.nix;
 
           # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
         }
