f166b0ec054634b605877c7f89c4bf8a7938717e
Alikay <1401288@gmail.com>
2024-05-24 22:43:11 -0600

    Automatic commit from rebuild script

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -211,6 +211,16 @@
   virtualisation.waydroid.enable = true;
   programs.virt-manager.enable = true;
   
+  # Appimage Support
+  boot.binfmt.registrations.appimage = {
+		wrapInterpreterInShell = false;
+		interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+		recognitionType = "magic";
+		offset = 0;
+		mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+		magicOrExtension = ''\x7fELF....AI\x02'';
+	};
+  
   # Flatpaks
   services.flatpak.enable = true;
   services.flatpak.remotes = [{
@@ -267,6 +277,7 @@
      foot
      pkgs-stable.aseprite
      amberol
+     appimage-run
      bear
      btop
      blueberry
