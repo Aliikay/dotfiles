b969b8d9877fdf86db7378ce30b4fda386770ad3
Alikay <1401288@gmail.com>
2024-09-13 23:39:00 -0600

    Auto commit from manual change: Generation 99

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -377,7 +377,7 @@
     "xyz.ketok.Speedtest"
     "io.github.RodZill4.Material-Maker"
     "io.github.zen_browser.zen"
-    "com.github.treagod.spectator"
+    "es.danirod.Cartero"
   ];
   
   # Flatpak auto updates
