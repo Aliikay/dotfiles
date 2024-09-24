d5316fa14ca9e1f5a5fc91a69644dc1e35847dab
Alikay <1401288@gmail.com>
2024-09-24 00:19:02 -0600

    Auto commit from manual change: Generation 117

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -143,19 +143,19 @@
   services.tlp = {
   		enable = true;
   		settings = {
-  			CPU_SCALING_GOVERNOR_ON_AC = "powersave";#"performance";
+  			CPU_SCALING_GOVERNOR_ON_AC = "performance";
 		    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 				
-				CPU_ENERGY_PERF_POLICY_ON_AC = "power";#"performance";
+				CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
 		    CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
 		    CPU_MIN_PERF_ON_AC = 0;
-		    CPU_MAX_PERF_ON_AC = 50;
+		    CPU_MAX_PERF_ON_AC = 100;
 		    CPU_MIN_PERF_ON_BAT = 0;
-		    CPU_MAX_PERF_ON_BAT = 30;
+		    CPU_MAX_PERF_ON_BAT = 50;
 		    
 		    CPU_BOOST_ON_AC = 1;
-      	CPU_BOOST_ON_BAT = 0;
+        CPU_BOOST_ON_BAT = 0;
 
 		    #Optional helps save long term battery health
 		    START_CHARGE_THRESH_BAT0 = 80; # 80 and bellow it starts to charge
