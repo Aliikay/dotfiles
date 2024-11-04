8af85d04e4140cacd15cb88942e8d32cd93b74b4
Alikay <1401288@gmail.com>
2024-11-04 14:35:31 -0700

    Auto commit from manual change: Generation 167

--- nixos/modules/lsp.nix
+++ nixos/modules/lsp.nix
@@ -10,5 +10,7 @@
     nixd # nix
     nodePackages.typescript-language-server # JS and Typescript
     rust-analyzer #Rust
+    tailwindcss-language-server #CSS
+    vscode-langservers-extracted #HTML, CSS, JSON, esLint
   ];
 }
