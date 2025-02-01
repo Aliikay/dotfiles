7c054f075a4bd4c8e3ecab22717d093f6af019e3
Alikay <1401288@gmail.com>
2025-02-01 16:07:51 -0700

    Auto commit from manual change: Generation 273

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -92,6 +92,14 @@
     "/share/nautilus-python/extensions"
   ];
 
+  # Fix missing gstreamer plugins for nautilus (audio / video file properties)
+  #environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" [
+  #  pkgs.gst_all_1.gst-plugins-good
+  #  pkgs.gst_all_1.gst-plugins-bad
+  #  pkgs.gst_all_1.gst-plugins-ugly
+  #  pkgs.gst_all_1.gst-plugins-libav
+  #];
+
   # Enable flakes
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
