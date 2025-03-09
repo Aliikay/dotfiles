5ad2fe9ef24fca835f6955ce0a44affe15c87e07
Alikay <1401288@gmail.com>
2025-03-09 03:39:55 -0600

    Auto commit from manual change: Generation 341

--- nixos/alikay-home.nix
+++ nixos/alikay-home.nix
@@ -187,6 +187,30 @@
     ];
   };
 
+  # MPV Scripts
+  programs.mpv = {
+    enable = true;
+
+    package = (
+      pkgs.mpv-unwrapped.wrapper {
+        scripts = with pkgs.mpvScripts; [
+          uosc
+          mpris
+        ];
+
+        mpv = pkgs.mpv-unwrapped.override {
+          waylandSupport = true;
+        };
+      }
+    );
+
+    config = {
+      profile = "high-quality";
+      ytdl-format = "bestvideo+bestaudio";
+      cache-default = 4000000;
+    };
+  };
+
   # Ghostty
   programs.ghostty = {
     enable = true;
