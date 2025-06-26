9841e8a4ae0142d36e4dd4c6681477cf85dadb51
Alikay <1401288@gmail.com>
2025-06-25 23:28:05 -0600

    Auto commit from manual change: Generation 461

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -471,6 +471,7 @@ in {
     [
       corefonts
       vistafonts
+      google-fonts
       maple-mono.truetype
     ]
     ++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts); # include all nerdfonts! maybe i can trim this down later
