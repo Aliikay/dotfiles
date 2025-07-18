003fbcbb4c8e8422e3457fa7de42bb6964b3302a
Alikay <1401288@gmail.com>
2025-07-17 20:44:05 -0600

    Auto commit from manual change: Generation 489

--- nixos/modules/samba.nix
+++ nixos/modules/samba.nix
@@ -28,7 +28,7 @@
         "path" = "/home/alikay/Public";
         "browseable" = "yes";
         "read only" = "no";
-        "guest ok" = "yes";
+        "guest ok" = "no";
         "create mask" = "0644";
         "directory mask" = "0755";
         "force user" = "username";
