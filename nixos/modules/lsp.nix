3369c4c545ee2aa2612160a6d5c25b78e7d36be1
Alikay <1401288@gmail.com>
2025-08-09 00:28:21 -0600

    Auto commit from manual change: Generation 518

--- nixos/modules/lsp.nix
+++ nixos/modules/lsp.nix
@@ -9,7 +9,7 @@
   environment.systemPackages = with pkgs; [
     nixd # nix
     nodePackages.typescript-language-server # JS and Typescript
-    rust-analyzer #Rust
+    pkgs-unstable.rust-analyzer #Rust
     tailwindcss-language-server #CSS
     vscode-langservers-extracted #HTML, CSS, JSON, esLint
     omnisharp-roslyn # CSharp
