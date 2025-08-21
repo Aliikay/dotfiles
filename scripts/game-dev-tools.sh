bb39f41e2df5cee7833c232f0d0d7a6259fc7a3c
Alikay <1401288@gmail.com>
2025-08-20 20:46:21 -0600

    Auto commit from manual change: Generation 524

--- scripts/game-dev-tools.sh
+++ scripts/game-dev-tools.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-unityhub & disown
+#unityhub & disown
+Unity/Hub/Editor/6000.0.37f1/Editor/Unity -projectPath ~/Documents/Unity/UntitledBubbleGame/Slipstream -force-vulkan -force-gfx-mt & disown
 mullvad split-tunnel add $!
 
 code & disown
