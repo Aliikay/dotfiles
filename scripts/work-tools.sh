cceace98ad7bbc6d396429051565a20abf386375
Alikay <1401288@gmail.com>
2025-09-25 01:31:48 -0600

    Auto commit from manual change: Generation 567

--- scripts/work-tools.sh
+++ scripts/work-tools.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Open work browser
-flatpak run app.zen_browser.zen -P Work
+flatpak run app.zen_browser.zen -P Work & disown
 
 # Open application spreadsheet
-libreoffice /home/alikay/Documents/Notes/Personal\ Notes/Work/Sent\ Applications.ods
+libreoffice /home/alikay/Documents/Notes/Personal\ Notes/Work/Sent\ Applications.ods & disown
 
 # Open obsidian
-flatpak run md.obsidian.Obsidian
+flatpak run md.obsidian.Obsidian & disown
