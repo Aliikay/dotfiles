a1ac6950d7a229547a38b948051e6f3bac2f6f1b
Alikay <1401288@gmail.com>
2025-10-09 17:31:40 -0600

    Auto commit from manual change: Generation 574

--- scripts/work-tools.sh
+++ scripts/work-tools.sh
@@ -2,6 +2,7 @@
 
 # Open work browser
 flatpak run app.zen_browser.zen -P Work & disown
+mullvad split-tunnel add $!
 
 # Open application spreadsheet
 libreoffice /home/alikay/Documents/Notes/Personal\ Notes/Work/Sent\ Applications.ods & disown
