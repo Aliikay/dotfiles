d138c18442d6663b32a9a5e3dc10b7f1e7364199
Alikay <1401288@gmail.com>
2025-11-22 17:49:38 -0700

    Auto commit from manual change: Generation 610

--- flake.nix
+++ flake.nix
@@ -166,6 +166,7 @@
         ./nixos/modules/emulation.nix
         ./nixos/modules/audio-production.nix
         ./nixos/modules/samba.nix
+        ./nixos/modules/vr.nix
         #./nixos/modules/cmput-325.nix
 
         # Disable this too to avoid errors without the secrets flake
---
+++ nixos/modules/vr.nix
@@ -0,0 +1,20 @@
+{
+  config,
+  lib,
+  pkgs,
+  pkgs-unstable,
+  pkgs-stable,
+  pkgs-last-stable,
+  inputs,
+  ...
+}: {
+  services.monado = {
+    enable = true;
+    defaultRuntime = true; # Register as default OpenXR runtime
+  };
+
+  systemd.user.services.monado.environment = {
+    STEAMVR_LH_ENABLE = "1";
+    XRT_COMPOSITOR_COMPUTE = "1";
+  };
+}
