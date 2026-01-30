de82289d9e4e6684b6461ddfb77027a3ce4fc34d
Alikay <1401288@gmail.com>
2026-01-30 02:17:09 -0700

    Auto commit from manual change: Generation

--- scripts/launch-picocad.sh
+++ scripts/launch-picocad.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 Xwayland :10 &
 sleep 1
-DISPLAY=:10 env -u WAYLAND_DISPLAY openbox --startup "bash -c '/home/alikay/Applications/PicoCAD/picocad-102/picocad-102.bin/linux/picocad & thunar'" &
+DISPLAY=:10 env -u WAYLAND_DISPLAY openbox --startup "bash -c '/home/alikay/Applications/PicoCAD/picocad-102/picocad-102.bin/linux/picocad & thunar /home/alikay/Applications/PicoCAD'" &
