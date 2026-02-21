bf339415a69e2c0ab4e6b827d3e7c1292582066e
Alikay <1401288@gmail.com>
2026-02-21 04:53:22 -0700

    Auto commit from manual change: Generation

--- scripts/screen-record.sh
+++ scripts/screen-record.sh
@@ -3,9 +3,9 @@
 if pgrep [w]l-screenrec
 then
     pkill wl-screenrec
-    notify-send --expire-time=3000 --transient "Finished screen recording" "Recording was saved to ~/Videos/Screencasts"
+    notify-send --icon=/home/alikay/dotfiles/icons/screen-record.png --expire-time=3000 --transient "Finished screen recording" "Recording was saved to ~/Videos/Screencasts"
 else
-    notify-send --expire-time=2000 --transient "Starting a screen recording" "Recording will be saved to ~/Videos/Screencasts"
+    notify-send --icon=/home/alikay/dotfiles/icons/screen-record.png --expire-time=3000 --transient "Starting a screen recording" "Toggle with Meta+Ctrl+Shift+S"
     play -v 0.1 ~/Music/SFX/screen-record.wav
     wl-screenrec -g "$(slurp)" -f ~/Videos/Screencasts/$(date +'%s_vid.mp4')
 fi
