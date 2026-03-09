ce2a41251031a27fa22d90f0894ef61793541da1
Alikay <1401288@gmail.com>
2026-03-09 06:51:46 -0600

    Auto commit from manual change: Generation

--- scripts/launch-swayidle.sh
+++ scripts/launch-swayidle.sh
@@ -2,6 +2,6 @@
 
 swayidle -w \
     before-sleep swaylock \
-    timeout 1140 'notify-send --transient "Idle Screen Lock" "Locking the screen in 1 minute!"' \
+    timeout 1140 'notify-send --transient --expire-time=60000 "Idle Screen Lock" "Locking the screen in 1 minute!"' \
         resume 'notify-send --transient "Idle Screen Lock" "Cancelled screen lock"' \
     timeout 1200 'niri msg action power-off-monitors && swaylock'
