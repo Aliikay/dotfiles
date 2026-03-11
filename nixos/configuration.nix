612317ff9a2607d51eb8f426e11b5e32e61b781c
Alikay <1401288@gmail.com>
2026-03-11 05:59:18 -0600

    Auto commit from manual change: Generation

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -164,6 +164,19 @@ in {
   # Enable ThermalD
   services.thermald.enable = true;
 
+  # Enable auto-cpufreq
+  # services.auto-cpufreq.enable = true;
+  # services.auto-cpufreq.settings = {
+  #   battery = {
+  #     governor = "powersave";
+  #     turbo = "never";
+  #   };
+  #   charger = {
+  #     governor = "performance";
+  #     turbo = "auto";
+  #   };
+  # };
+
   # Enable TLP
   services.tlp = {
     enable = false;
