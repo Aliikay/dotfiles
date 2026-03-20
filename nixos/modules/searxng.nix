112e7a16545e84cd9956618305670e974d29b850
Alikay <1401288@gmail.com>
2026-03-19 18:04:30 -0600

    Auto commit from manual change: Generation

--- nixos/modules/searxng.nix
+++ nixos/modules/searxng.nix
@@ -14,8 +14,8 @@
     environmentFile = "/home/alikay/.searxng.env";
     settings = {
       server = {
-        bind_address = "::1";
-        #port = 6767;
+        bind_address = "127.0.0.1";
+        port = 6767;
         # WARNING: setting secret_key here might expose it to the nix cache
         # see below for the sops or environment file instructions to prevent this
         # secret_key =
