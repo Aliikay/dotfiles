c305fb25ca47eb3cafa97b0e59548dd2f5a0f864
Alikay <1401288@gmail.com>
2026-03-26 04:38:42 -0600

    Auto commit from manual change: Generation

--- scripts/work-tools.sh
+++ scripts/work-tools.sh
@@ -13,5 +13,11 @@ flatpak run md.obsidian.Obsidian & disown
 # Open file browser
 nautilus /home/alikay/Documents/Notes/Personal\ Notes/Work & disown
 
+# Open file browser
+nautilus /home/alikay/Documents/CV & disown
+
+# Open git
+alacritty --working-directory ~/Documents/CV -c lazygit
+
 # Open resume
 setzer & disown
