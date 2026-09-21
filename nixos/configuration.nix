9ba59abb99cf391566ec28fafd392d7579b4290d
Alikay <1401288@gmail.com>
2026-09-20 20:24:30 -0600

    Auto commit from manual change: Generation

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -547,13 +547,17 @@ in {
   users.defaultUserShell = pkgs.fish;
 
   # Appimage Support
-  boot.binfmt.registrations.appimage = {
-    wrapInterpreterInShell = false;
-    interpreter = "${pkgs.appimage-run}/bin/appimage-run";
-    recognitionType = "magic";
-    offset = 0;
-    mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
-    magicOrExtension = ''\x7fELF....AI\x02'';
+  # boot.binfmt.registrations.appimage = {
+  #   wrapInterpreterInShell = false;
+  #   interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+  #   recognitionType = "magic";
+  #   offset = 0;
+  #   mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+  #   magicOrExtension = ''\x7fELF....AI\x02'';
+  # };
+  programs.appimage = {
+    enable = true;
+    binfmt = true;
   };
 
   # Making alacritty the default terminal
